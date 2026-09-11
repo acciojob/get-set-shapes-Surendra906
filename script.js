46f84c368bb17aa3aba2b7fd940f7abd798fecdf
Surendra906 <surendramode945@gmail.com>
2026-09-11 11:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ class Rectangle {
 
 class Square extends Rectangle {
 	 constructor(side){
-		 super(side,side){
+		 super(side);
 			 this._side=side;
-		 }
+		 
 	 }
 	getPerimeter(){
 		return 4*this._side;
